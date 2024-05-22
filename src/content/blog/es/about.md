4b149bf75639fd478f07c8168258d09010a518be
Grant Richmond <mail@grant.codes>
2024-05-22 14:17:15 +0200

    improve i18n

---
+++ src/content/blog/es/about.md
@@ -0,0 +1,47 @@
+---
+title: Astro en breve
+author: Himanshu
+description: Find out what makes Astro awesome!
+date: 2024-04-28
+tags: []
+---
+
+# Echidnae causa igitur vidi est mea regina
+
+## Parce donec dives eripiunt cuperet possent in
+
+Lorem markdownum reieci inpiger Achivos eversae properas villo leonibus et sic
+tibi melioribus Olenios membris sisto domus fervens versabat. Fuerat **et
+timidum reliquit** dabat ludit ulterius ramis.
+
+## Panthoides fuit
+
+Audiat relinquit reddit patriam terrificam lassata. Arcus quam quem collo.
+Recisum lingua si bis Iovis nova Procne fretum indice pressanda.
+
+Verbis ruit tenet parvae qui ipse postquam, **aequor**, ergo nomen oculisque,
+cecidit. Vitae quid suis [micantes](http://manumque-telamonque.com/tuad)!
+
+## Est esse omnia vulneris
+
+Hoc tamen, arva diu Styx ministros? Demit cum, aristas Eurynomus; cervice loca
+Naxon et rex fugae rear. Est multo. Peteret in mutavit fratres, sic utroque
+adhuc.
+
+## Fronde est
+
+Egeriae nemorisque antra, ac arbor ad mandata, hic faceres amores sceleratae
+inter ab. Corque in fecit silvisque levavit in altaque Helles, tumulus, Romana,
+et. Vitta pondere Stygia, nepos lactis et redit te
+[parentem](http://corpora-nitentem.org/sed.php) et _Acheloe_ magna, temporis et
+nefanda freta! Novi tum fecere saepius ingenium illis aristas non, si gelidi!
+Largi si per; sensit, stipata Argos, satyri!
+
+- Capit vive vasti dominam ante ignes sub
+- Coeamus illos mihi
+- Quam evocat Athenae quid sparsus dedere
+- Pomis rubet praebebatque molis
+
+Huc fratrum iam: longius ad ferro, oris veniat; pronus arma cursus. Vel dum
+mugitus corona et tuta quin fugiunt semper **in horrenda scindunt** rexit dixere
+erant dextra revinctam querellis, ora.
--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { ui, defaultLang, showDefaultLang } from './ui'
+import { ui, languages, defaultLang, showDefaultLang } from './ui'
 
 export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/')
@@ -16,6 +16,50 @@ export function useTranslatedPath(lang: keyof typeof ui) {
   return function translatePath(path: string, l: string = lang) {
     return !showDefaultLang && l === defaultLang ? path : `/${l}${path}`
   }
+}
+
+export function getEntryStaticPaths(entries: any[]): any[] {
+  const staticPaths = entries.map((entry) => {
+    let lang = defaultLang
+    let slug = [entry.slug]
+    if (entry.slug.includes('/')) {
+      // Is in a language subdirectory
+      const [entryLang, ...entrySlug] = entry.slug.split('/')
+      if (
+        Object.keys(languages).includes(entryLang) &&
+        entryLang !== defaultLang
+      ) {
+        lang = entryLang
+        slug = entrySlug
+      }
+    }
+    return {
+      params: { lang, slug: slug.join('/') || undefined },
+      props: { entry },
+    }
+  })
+
+  return staticPaths
+}
+
+export function entryMatchesLang(entry: any, lang: string) {
+  // If not a default language, then it will be in a language subdirectory.
+  if (lang !== defaultLang) {
+    return entry.slug.startsWith(`${lang}/`)
+  }
+
+  // If it is the default language, then it will not be in a language subdirectory.
+  return !Object.keys(languages).some((lang) =>
+    entry.slug.startsWith(`${lang}/`)
+  )
+}
+
+export function getEntryLangFilter(lang: string) {
+  return (entry: any) => entryMatchesLang(entry, lang)
+}
+
+export function filterEntriesByLang(entries: any[], lang: string) {
+  return entries.filter((entry) => entryMatchesLang(entry, lang))
 }
 
 export { defaultLang }
